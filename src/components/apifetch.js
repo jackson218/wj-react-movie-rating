56148148f8a82cdcdaf0522fc8e88685044ddd49
jackson218 <wbjackson218@gmail.com>
2018-12-12 10:25:14 -0700

    first commit

---
+++ src/components/apifetch.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+export default class Apifetch extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            movies: []
+        }
+    }
+
+    componentWillMount() {
+        fetch("http://127.0.0.1:5000/return/movies", {
+            method: "GET",
+            headers: {
+                "accept": "application/json",
+                "Content-Type": 'application/json'
+            }
+        })
+            .then(response => { return response.json(); })
+            .then(responseData => { return responseData })
+            .then(data => { this.setState({ movies: data }); })
+
+            .catch(err => {
+                console.log('Fetch error' + err)
+            })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="fetch-inner-con">
+                    <h1>Here are the current movies</h1>
+                    <div className="card-spacing">
+                        {this.state.movies.map((data, index) => (
+                            <div key={index} className="movie-card">
+                                <p>Title: {data[0]}</p>
+                                <p>Rating: {data[1]}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Movie Services</h1>
+      </div>
+    );
+  }
+}
